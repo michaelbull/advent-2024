25a0e7050d004c5b094415c8f0c155aa2c8d6235
Michael Bull <mikebull94@gmail.com>
2024-12-02 13:48:45 +0000

    Simplify day 1 solution

--- src/main/kotlin/com/github/michaelbull/advent2024/day01/Day1.kt
+++ src/main/kotlin/com/github/michaelbull/advent2024/day01/Day1.kt
@@ -7,28 +7,24 @@ private typealias IntPair = Pair<Int, Int>
 private typealias IntListPair = Pair<List<Int>, List<Int>>
 
 private fun String.toIntPair(): IntPair {
-    val parts = split("   ")
-    return parts[0].toInt() to parts[1].toInt()
+    val (left, right) = split("   ", limit = 2)
+    return left.toInt() to right.toInt()
 }
 
 private fun IntPair.difference(): Int {
     return abs(first - second)
 }
 
-private fun <T> Iterable<T>.countOf(element: T): Int = count { other ->
-    element == other
-}
-
-private fun List<Int>.similarityScore(element: Int): Int {
-    return element * countOf(element)
+private fun Map<Int, Int>.similarityScore(element: Int): Int {
+    return element * getOrDefault(element, 0)
 }
 
 object Day1 : Puzzle<IntListPair, Int>(day = 1) {
 
     override fun parse(lines: Sequence<String>): IntListPair {
         val pairs = lines.map(String::toIntPair).toList()
-        val left = pairs.map(IntPair::first).sorted()
-        val right = pairs.map(IntPair::second).sorted()
+        val left = pairs.map(IntPair::first)
+        val right = pairs.map(IntPair::second)
         return left to right
     }
 
@@ -40,12 +36,15 @@ object Day1 : Puzzle<IntListPair, Int>(day = 1) {
     fun part1(input: IntListPair): Int {
         val (left, right) = input
 
-        return left.zip(right).sumOf(IntPair::difference)
+        return left.sorted()
+            .zip(right.sorted())
+            .sumOf(IntPair::difference)
     }
 
     fun part2(input: IntListPair): Int {
         val (left, right) = input
+        val frequencies = right.groupingBy { it }.eachCount()
 
-        return left.sumOf(right::similarityScore)
+        return left.sumOf(frequencies::similarityScore)
     }
 }
