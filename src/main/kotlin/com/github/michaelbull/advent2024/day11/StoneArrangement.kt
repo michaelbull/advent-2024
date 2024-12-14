85e2086916b41c7d5d0e455d374342ce8bfe4070
Michael Bull <mikebull94@gmail.com>
2024-12-14 00:58:11 +0000

    Model MutableStoneCountMap in day 11

--- src/main/kotlin/com/github/michaelbull/advent2024/day11/StoneArrangement.kt
+++ src/main/kotlin/com/github/michaelbull/advent2024/day11/StoneArrangement.kt
@@ -3,7 +3,8 @@ package com.github.michaelbull.advent2024.day11
 import kotlin.math.log10
 import kotlin.math.pow
 
-private typealias StoneCountMap = MutableMap<Long, Long>
+private typealias StoneCountMap = Map<Long, Long>
+private typealias MutableStoneCountMap = MutableMap<Long, Long>
 
 fun String.toStoneArrangement(): StoneArrangement {
     return StoneArrangement(split(" ").map(String::toLong))
@@ -20,7 +21,7 @@ class StoneArrangement(
     }
 
     private fun blink(): Sequence<StoneCountMap> {
-        return generateSequence(stoneCounts.toMutableMap(), ::blink)
+        return generateSequence(stoneCounts, ::blink)
     }
 
     private fun blink(stoneCounts: StoneCountMap): StoneCountMap {
@@ -33,7 +34,7 @@ class StoneArrangement(
         return result
     }
 
-    private fun StoneCountMap.blink(stone: Long, count: Long) {
+    private fun MutableStoneCountMap.blink(stone: Long, count: Long) {
         val digits = stone.digitCount()
 
         when {
@@ -43,7 +44,7 @@ class StoneArrangement(
         }
     }
 
-    private fun StoneCountMap.split(stone: Long, count: Long) {
+    private fun MutableStoneCountMap.split(stone: Long, count: Long) {
         val divisor = 10.0.pow(stone.digitCount() / 2).toLong()
         val left = stone % divisor
         val right = stone / divisor
@@ -52,7 +53,7 @@ class StoneArrangement(
         mergeAdditively(right, count)
     }
 
-    private fun StoneCountMap.mergeAdditively(stone: Long, count: Long) {
+    private fun MutableStoneCountMap.mergeAdditively(stone: Long, count: Long) {
         merge(stone, count, Long::plus)
     }
 
